681620f8eed9e7abded265e5b506cee59adaa96f
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2023-11-20 14:36:10 +0000

    styles added

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello World')
+let homeScore = 0;
+let guestScore = 0;
+
+let homeScoreDisplay = document.getElementById("home-score");
+let guestScoreDisplay = document.getElementById("guest-score");
+
+let homeAddOne = document.getElementById("board1-btn1");
+let homeAddTwo = document.getElementById("board1-btn2");
+let homeAddThree = document.getElementById("board1-btn3");
+
+let guestAddOne = document.getElementById("board2-btn1");
+let guestAddTwo = document.getElementById("board2-btn2");
+let guestAddThree = document.getElementById("board2-btn3");
+
+
