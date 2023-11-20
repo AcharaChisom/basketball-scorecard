fe8dec1becb88081de99d056fb203e0546752109
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2023-11-20 15:01:23 +0000

    interactivity added

--- index.js
+++ index.js
@@ -12,4 +12,44 @@ let guestAddOne = document.getElementById("board2-btn1");
 let guestAddTwo = document.getElementById("board2-btn2");
 let guestAddThree = document.getElementById("board2-btn3");
 
+let reset = document.getElementById("reset-btn");
 
+
+reset.addEventListener("click", function () {
+    homeScore = 0;
+    guestScore = 0;
+    homeScoreDisplay.innerHTML = homeScore;
+    guestScoreDisplay.innerHTML = guestScore;
+});
+
+
+homeAddOne.addEventListener("click", () => {
+    homeScore++;
+    homeScoreDisplay.textContent = homeScore;
+});
+
+homeAddTwo.addEventListener("click", () => {
+    homeScore += 2;
+    homeScoreDisplay.textContent = homeScore;
+});
+
+homeAddThree.addEventListener("click", () => {
+    homeScore += 3;
+    homeScoreDisplay.textContent = homeScore;
+});
+
+
+guestAddOne.addEventListener("click", () => {
+    guestScore++;
+    guestScoreDisplay.textContent = guestScore;
+});
+
+guestAddTwo.addEventListener("click", () => {
+    guestScore += 2;
+    guestScoreDisplay.textContent = guestScore;
+});
+
+guestAddThree.addEventListener("click", () => {
+    guestScore += 3;
+    guestScoreDisplay.textContent = guestScore;
+});
